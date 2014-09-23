a98f0f0b95d8846a80dfa4e16225461cfb28a6e8
Taylor Sullivan <taylor.sullivan@me.com>
2014-09-23 10:46:11 -0700

    flesh out user and session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,33 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  helper_method :current_user, :signed_in?
+  
+  def sign_in!(user)
+    user.reset_session_token!
+    session[:session] = user.session_token
+  end
+  
+  def sign_out!
+    current_user.try(:reset_session_token!)
+    session[:session] = nil
+  end
+  
+  def current_user
+    return nil unless session[:session]
+    
+    @current_user ||= User.find_by_session_token(session[:session])
+  end
+  
+  def signed_in?
+    !!current_user
+  end
+  
+  def require_authenticated!
+    unless signed_in?
+      flash[:errors] = ["You must be signed in to perform that action."]
+      redirect_to new_session_url
+    end
+  end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_credentials(session_params[:email_address],
+                                    session_params[:password])
+                                    
+    if user
+      sign_in!(user)
+      redirect_to root_url
+    else
+      flash.now[:errors] = ["Invalid username or password."]
+      render :new
+    end
+  end
+
+  def destroy
+    sign_out!
+  end
+  
+  private
+  
+  def session_params
+    params.require(:session).permit(:email_address, :password)
+  end
+end
